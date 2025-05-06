fb3260a13cf28a6fcf8b8ed0f73ec5cf99e27282
jetonpeche <nicolas.np63@gmail.com>
2025-05-06 16:33:58 +0200

    Delete ButtonModule.cs

--- botDiscord/ButtonModule.cs
+++
@@ -1,9 +0,0 @@
-﻿using NetCord.Services.ComponentInteractions;
-
-namespace ecoleIA;
-
-public class ButtonModule: ComponentInteractionModule<ButtonInteractionContext>
-{
-    [ComponentInteraction("button_quizz")]
-    public static string Button() => "You clicked a button!";
-}
